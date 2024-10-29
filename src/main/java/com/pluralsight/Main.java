419f2e61c10b4c76bf24271aaade39048fd34804
Osmig_Torres <torresosmig@gmail.com>
2024-10-29 11:05:05 -0700

    initial commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Moped slowRide = new Moped();
+        slowRide.setColor("Red");
+        slowRide.setFuelCapacity(5);
+
+        System.out.println(slowRide.getColor());
+
+    }
+}
